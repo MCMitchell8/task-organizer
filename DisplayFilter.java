78bf04c42eda373df4bb2ba752543788cae31db3
Melissa M <65644728+MCMitchell8@users.noreply.github.com>
2020-07-19 19:07:18 -0700

    Display filter code

---
+++ DisplayFilter.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TaskOrganization;
+
+/**
+ *
+ * @author melissacmitchell
+ */
+//enum defines the types of transaction
+public enum DisplayFilter {
+    Both, Single, Repeating
+}
