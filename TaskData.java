aa45b77edd6ad8ce2180c7daef7fe7df24b9a525
Melissa M <65644728+MCMitchell8@users.noreply.github.com>
2020-07-24 17:12:15 -0700

    Task data code

---
+++ TaskData.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TaskOrganization;
+
+/**
+ *
+ * @author melissacmitchell
+ */
+public class TaskData {
+    //variables to hold the user's task data
+    double totalTasks = 0;
+    double homework = 0;
+    double housework = 0;
+    double piano = 0;
+    double crafts = 0;
+    double miscellaneous = 0;
+}
